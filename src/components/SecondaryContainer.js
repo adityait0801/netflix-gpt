08d76a21674a5583c7a3a8556777c1ef20c93da7
Aditya Singh <aditya.it0801@gmail.com>
2024-06-01 11:00:36 +0530

    Movie Card

--- src/components/SecondaryContainer.js
+++ src/components/SecondaryContainer.js
@@ -1,6 +1,13 @@
+import { useSelector } from "react-redux";
+import MovieList from "./MovieList";
+
 const SecondaryContainer = () => {
+
+    const movies = useSelector((store)=> store.movies.NowPlayingMovies)
+
     return <div>
         SecondaryContainer
+        <MovieList title={"Now Playing"} movies={movies.nowPlaying}/>
     </div>
 }
 
